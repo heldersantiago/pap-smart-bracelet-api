2a811ff84de74c51317cd22423142697485a0789
heldersantiago <heldersantiago273@gmail.com>
2024-02-29 00:22:47 +0100

    feat: adding the DB Diagram

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,4 +1,3 @@
-import { UpdateOptions } from "sequelize";
 import { IUserService } from "../interfaces/IUserService";
 import { User } from "../models/user";
 import { IUser } from "../types/User";
@@ -15,6 +14,7 @@ export class UserService implements IUserService {
     }
     return user;
   }
+  
   //   public async updateUser(user: IUser, options: UpdateOptions): Promise<IUser> {
   //     const updatedUser = await User.update(user, options);
   //     return updatedUser;
