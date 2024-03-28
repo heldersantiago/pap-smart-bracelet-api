3bb3da8bb625e742d2b9ebeb2513e51b23cc2d3c
heldersantiago <heldersantiago273@gmail.com>
2024-03-28 23:39:20 +0100

    feat: create elderly finished

---
+++ src/routes/elderly.ts
@@ -0,0 +1,21 @@
+import { ElderlyController } from "../controllers/ElderlyController";
+import authorize from "../middlewares/authorize";
+import { BraceletController } from "../controllers/BraceletController";
+
+export class ElderlyRoutes {
+  private readonly apiUrl: string = "/api/v1/users/elderlies";
+  public elderlyController: ElderlyController = new ElderlyController();
+  public braceletController: BraceletController = new BraceletController();
+
+  public routes(app: any): void {
+    app
+      .route(this.apiUrl)
+      .get(this.elderlyController.index)
+      .post(this.elderlyController.create);
+    app
+      .route(this.apiUrl + "/:id")
+      .get(this.elderlyController.show)
+      .put(this.elderlyController.update)
+      .delete(this.elderlyController.destroy);
+  }
+}
