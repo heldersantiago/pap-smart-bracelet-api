ad9c9c77583fb839583534d26e0e615b77340d21
heldersantiago <heldersantiago273@gmail.com>
2024-04-03 01:25:25 +0100

    feat: add documentation for elderlies yet

--- src/routes/elderly.ts
+++ src/routes/elderly.ts
@@ -1,5 +1,4 @@
 import { ElderlyController } from "../controllers/ElderlyController";
-import authorize from "../middlewares/authorize";
 import { BraceletController } from "../controllers/BraceletController";
 
 export class ElderlyRoutes {
@@ -8,10 +7,61 @@ export class ElderlyRoutes {
   public braceletController: BraceletController = new BraceletController();
 
   public routes(app: any): void {
-    app
-      .route(this.apiUrl)
-      .get(this.elderlyController.index)
-      .post(this.elderlyController.create);
+    /**
+     * @openapi
+     * /users/elderlies:
+     *   get:
+     *     summary: Get all Elderlies Users
+     *     description: Get all Elderlies.
+     *     responses:
+     *       '200':
+     *         description: A successful response
+     *       '500':
+     *         description: Internal server error
+     */
+    app.get(this.apiUrl, this.elderlyController.index);
+    /**
+     * @openapi
+     * /users/elderlies:
+     *   post:
+     *     summary: Create a new Elderly User
+     *     description: Creates a new elderly user.
+     *     requestBody:
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema:
+     *             type: object
+     *             properties:
+     *               name:
+     *                 type: string
+     *                 description: The name of the elderly user.
+     *               email:
+     *                 type: string
+     *                 description: The name of the elderly user.
+     *               password:
+     *                 type: string
+     *                 description: The name of the elderly user.
+     *               phone:
+     *                 type: string
+     *                 description: The name of the elderly user.
+     *               role_id:
+     *                 type: integer
+     *                 description: The name of the elderly user.
+     *             required:
+     *               - name
+     *               - email
+     *               - password
+     *               - phone
+     *               - role_id
+     *     responses:
+     *       '200':
+     *         description: A successful response
+     *       '500':
+     *         description: Internal server error
+     */
+
+    app.post(this.apiUrl, this.elderlyController.create);
     app
       .route(this.apiUrl + "/:id")
       .get(this.elderlyController.show)
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import * as dotenv from "dotenv";
 import path = require("path");
 import app from "./app";
+import swaggerUi from "swagger-ui-express";
+import { swaggerOptions } from "./config/swagger";
+import swaggerJSDoc = require("swagger-jsdoc");
 
 // Setting up the environment
 const envPATH = path.resolve(__dirname, "../.env");
@@ -8,4 +11,8 @@ dotenv.config({ path: envPATH });
 
 const PORT = Number(process.env.PORT) || 3001;
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+const swaggerDocs = swaggerJSDoc(swaggerOptions);
+
+app.use("/api/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+app.listen(PORT, () => console.info(`API app listening on port ${PORT}!`));
